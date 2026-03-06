4517377799ea6e667e2f614a2302588019010401
adguhan <adguhan@iu.edu>
2026-03-06 10:49:00 -0500

    Add Tampermonkey userscript

---
+++ kumonextensions.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         kumonextensions
+// @namespace    https://github.com/Invisibl5/kumonextensions
+// @version      0.1.0
+// @description  Browser enhancements for Kumon-related pages
+// @author       Invisibl5
+// @match        *://*/*
+// @run-at       document-idle
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/Invisibl5/kumonextensions/main/kumonextensions.user.js
+// @downloadURL  https://raw.githubusercontent.com/Invisibl5/kumonextensions/main/kumonextensions.user.js
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // TODO: Add your Kumon-specific logic here.
+  // Example placeholder:
+  console.log('[kumonextensions] userscript loaded');
+})();
+
